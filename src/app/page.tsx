bbcc70c516933dd051a992ccaeae1bd43dd1f768
juny-0429 <juny_0429@naver.com>
2025-02-12 20:59:57 +0900

    폴더명 대소문자 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import { Tabs, TabsList, TabsTrigger, TabsContent } from 'src/components/tabs/tabs';
-
 export default function Home() {
-  return (
-    <div>
-      <Tabs defaultValue='home' className='w-full'>
-        <TabsList className='flex border-b'>
-          <TabsTrigger value='home'>홈</TabsTrigger>
-          <TabsTrigger value='profile'>프로필</TabsTrigger>
-          <TabsTrigger value='settings'>설정</TabsTrigger>
-          <TabsTrigger value='settings'>설정</TabsTrigger>
-          <TabsTrigger value='settings'>설정</TabsTrigger>
-          <TabsTrigger value='settings'>설정</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value='home'>
-          <p>홈 화면 내용</p>
-        </TabsContent>
-        <TabsContent value='profile'>
-          <p>프로필 화면 내용</p>
-        </TabsContent>
-        <TabsContent value='settings'>
-          <p>설정 화면 내용</p>
-        </TabsContent>
-      </Tabs>
-    </div>
-  );
+  return <div></div>;
 }
--- src/components/temp_button/CapsuleButton.tsx
+++
@@ -1,6 +0,0 @@
-import BaseButton, { ButtonProps } from './BaseButton';
-import clsx from 'clsx';
-
-export default function CapsuleButton(props: ButtonProps) {
-  return <BaseButton {...props} className={clsx('rounded-full', props.className)} />;
-}
