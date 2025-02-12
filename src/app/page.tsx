80bc076ee49ac83e48eed62a3bfc03dc9e79fd45
juny-0429 <juny_0429@naver.com>
2025-02-12 11:13:14 +0900

    테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import CapsuleButton from 'src/components/button/CapsuleButton';
-import LucideIcons from 'src/theme/lucideIcon';
-
 export default function Home() {
-  return (
-    <div>
-      <CapsuleButton height={32} color='cta'>
-        안녕하세요
-      </CapsuleButton>
-
-      <CapsuleButton height={32} color='cta' disabled>
-        안녕하세요
-      </CapsuleButton>
-
-      <CapsuleButton height={56} color='cta'>
-        <LucideIcons.Home size={24} />
-        안녕하세요
-      </CapsuleButton>
-    </div>
-  );
+  return <div></div>;
 }
