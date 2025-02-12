e42acd8bbb30e3c44df63cfd03a4f94cf9880273
juny-0429 <juny_0429@naver.com>
2025-02-12 17:50:46 +0900

    line button 제작

--- src/components/button/BaseButton.tsx
+++ src/components/button/BaseButton.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import React, { useMemo } from 'react';
 import { ColorType } from 'src/types/color.types';
 
-type ButtonHeight = 32 | 40 | 48 | 56;
+export type ButtonHeight = 32 | 40 | 48 | 56;
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   height: ButtonHeight;
@@ -12,7 +12,7 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
   rightIcon?: React.ReactNode;
 }
 
-const heightStyle: { [k in ButtonHeight]: string } = {
+export const heightStyle: { [k in ButtonHeight]: string } = {
   32: 'h-[32px] gap-[2px] px-3 rounded-[4px] text-body-12r',
   40: 'h-[40px] gap-[4px] px-4 rounded-[4px] text-body-14r',
   48: 'h-[48px] gap-[6px] px-4 rounded-[6px] text-body-16r',
@@ -20,13 +20,15 @@ const heightStyle: { [k in ButtonHeight]: string } = {
 };
 
 const colorStyles: Partial<Record<ColorType, string>> = {
-  gray: 'bg-gray-500 enabled:hover:bg-gray-600 disabled:opacity-30 disabled:cursor-not-allowed',
-  cta: 'bg-ui-cta enabled:hover:bg-ui-cta-hover disabled:opacity-30 disabled:cursor-not-allowed',
+  gray: 'bg-gray-500 enabled:hover:bg-gray-600',
+  cta: 'bg-ui-cta enabled:hover:bg-ui-cta-hover',
 };
+
+export const disabledStyles = 'disabled:opacity-30 disabled:cursor-not-allowed';
 
 export default function BaseButton({ height, color, leftIcon, rightIcon, children, className, ...restProps }: ButtonProps) {
   const buttonClass = useMemo(() => {
-    return clsx('flex justify-center items-center text-white', heightStyle[height], colorStyles[color], className);
+    return clsx('flex justify-center items-center text-white', heightStyle[height], colorStyles[color], disabledStyles, className);
   }, [height, color, className]);
 
   return (
