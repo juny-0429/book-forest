1e35836ff499baf509684f8e4e7adb8869af96f5
juny-0429 <juny_0429@naver.com>
2025-02-17 16:05:59 +0900

    error message 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import ErrorMessage from 'src/components/ErrorMessage/ErrorMessage';
 import TextInput from 'src/components/TextInput/TextInput';
 import LucideIcons from 'src/theme/lucideIcon';
 
 export default function Home() {
   return (
     <div className='w-[250px] border-state-error'>
-      <TextInput isDirty rightIcon={<LucideIcons.Search />} />
+      <TextInput className='w-[300px]' isDirty rightIcon={<LucideIcons.Search />} />
       <TextInput isError isDirty placeholder='테스트 중 입니다' />
       <TextInput placeholder='안녕하세요 테스트' disabled />
       <TextInput disabled />
+      <ErrorMessage className='font-bold'>sdfdsf</ErrorMessage>
     </div>
   );
 }
