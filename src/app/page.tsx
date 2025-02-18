4a353efa9e3ba7d4737d4627c95a0ec1de2cba88
juny-0429 <juny_0429@naver.com>
2025-02-19 00:54:27 +0900

    테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>aaaa</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-      <h1>sdfsdfsdf</h1>
-    </div>
-  );
+  return <div></div>;
 }
