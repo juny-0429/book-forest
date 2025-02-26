ef52f1c666b3393cfd6ca4e2aa701b817d6790a7
juny-0429 <juny_0429@naver.com>
2025-02-27 01:43:08 +0900

    feat: 회원 탈퇴 폴더 및 페이지 추가

---
+++ src/app/(main)/account/delete/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AccountDeletePage() {
+  return (
+    <div className='flex flex-col w-full gap-[60px]'>
+      <h2 className='text-title-24b text-ui-text-title'>회원탈퇴</h2>
+    </div>
+  );
+}
