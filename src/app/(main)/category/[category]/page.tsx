cf4ac20eed842c8f8c8c579ada51194429c81da9
juny-0429 <juny_0429@naver.com>
2025-02-28 00:04:35 +0900

    feat: 카테고리 페이지 제작

--- src/app/(main)/category/[category]/page.tsx
+++ src/app/(main)/category/[category]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { CATEGORY_PATHS } from './_data/mockCategories.data';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from 'src/components/Breadcrumb/Breadcrumb';
 
 interface CategoryPageProps {
   params: { category: string };
@@ -12,7 +13,23 @@ export default async function CategoryPage({ params }: CategoryPageProps) {
 
   return (
     <div>
-      <h1>{category} 카테고리</h1>
+      <h2 className='text-title-32b text-ui-text-title'>한국소설</h2>
+
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/'>홈</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/components'>소설</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>한국소설</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
     </div>
   );
 }
