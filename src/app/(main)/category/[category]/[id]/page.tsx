e4510559a8f766f1207487c45401665b0b19ffc2
juny-0429 <juny_0429@naver.com>
2025-02-28 23:41:09 +0900

    feat: 상세페이지 교환 안내 섹션 제작

---
+++ src/app/(main)/category/[category]/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReturnPolicy from './_components/ReturnPolicy';
+
+export default function page() {
+  return (
+    <div>
+      상세페이지
+      <ReturnPolicy />
+    </div>
+  );
+}
