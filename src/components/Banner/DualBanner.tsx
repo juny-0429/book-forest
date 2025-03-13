efb77bddeaa3ecb38ee23840590e0e74ae7ae4f7
juny-0429 <juny_0429@naver.com>
2025-03-14 04:45:05 +0900

    refactor: 광고 표시 추가

--- src/components/Banner/DualBanner.tsx
+++ src/components/Banner/DualBanner.tsx
@@ -12,7 +12,12 @@ export default function DualBanner() {
       {dualBannerList &&
         dualBannerList.map((banner, index) => (
           <Link key={index} href={banner.banner_link}>
-            <Image src={banner.banner_image_url} width={500} height={170} alt={`${banner.banner_name} banner image`} className='w-[500px] h-[170px] object-cover rounded-[10px]' />
+            <div className='relative'>
+              <Image src={banner.banner_image_url} width={500} height={170} alt={`${banner.banner_name} banner image`} className='w-[500px] h-[170px] object-cover rounded-[10px]' />
+              <div className='absolute left-[25px] bottom-[25px] flex justify-center items-center w-[26px] h-[14px] bg-gray-300 rounded-full'>
+                <span className='text-[10px] font-bold text-white'>AD</span>
+              </div>
+            </div>
           </Link>
         ))}
     </section>
