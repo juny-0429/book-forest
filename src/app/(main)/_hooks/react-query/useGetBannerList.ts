e12ed282d8b498b44b530d75f89aaa4a09e7bfa0
juny-0429 <juny_0429@naver.com>
2025-03-13 17:40:34 +0900

    refactor: api 타입 추가

--- src/app/(main)/_hooks/react-query/useGetBannerList.ts
+++ src/app/(main)/_hooks/react-query/useGetBannerList.ts
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { BannerPositionType } from 'src/types/bannerPosition.types';
+import { BannerListItemDto } from '../../_dtos/getBannerList.dto';
 
-const getBannerList = async (position: BannerPositionType) => {
+const getBannerList = async (position: BannerPositionType): Promise<BannerListItemDto[]> => {
   const response = await fetch(`/api/banner?position=${position}`, {
     method: 'GET',
     headers: {
@@ -10,8 +11,8 @@ const getBannerList = async (position: BannerPositionType) => {
   });
 
   if (!response.ok) throw new Error('배너리스트 조회에 실패했습니다.');
-
-  return response.json();
+  const result = await response.json();
+  return result.data;
 };
 
 export const useGetBannerList = (position: BannerPositionType) => {
