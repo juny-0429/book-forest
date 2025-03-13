a71ce0a3246e1f92f4ec4442a75409238f44a669
juny-0429 <juny_0429@naver.com>
2025-03-13 17:40:53 +0900

    refactor: 샘플 데이터 삭제 및 api 연결

--- src/components/Banner/MainBannerSwiper.tsx
+++ src/components/Banner/MainBannerSwiper.tsx
@@ -3,26 +3,21 @@
 import { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper/modules';
-import type { Swiper as SwiperClass } from 'swiper';
+import Link from 'next/link';
 import Image from 'next/image';
 import LucideIcons from 'src/theme/lucideIcon';
-import SampleBannerImg from '@/assets/images/sample-banner-1.png';
+import type { Swiper as SwiperClass } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { useGetBannerList } from 'src/app/(main)/_hooks/react-query/useGetBannerList';
 
-export default function MainBannerSwiper() {
+export default function mainBannerListSwiper() {
   const [isPlaying, setIsPlaying] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(1);
-  const totalSlides = 13;
   const swiperRef = useRef<SwiperClass | null>(null);
 
-  const sampleBannerList = Array.from({ length: 13 }, (_, index) => ({
-    id: index + 1,
-    image: SampleBannerImg,
-    title: `배너 ${index + 1}`,
-    url: 'https://github.com/juny-0429',
-  }));
+  const { data: mainBannerList, isLoading } = useGetBannerList('main');
 
   const handleSwiper = (swiper: SwiperClass) => {
     swiperRef.current = swiper;
@@ -42,7 +37,7 @@ export default function MainBannerSwiper() {
 
   return (
     <>
-      <div className='relative w-full max-w-[1000px] max-h-[300px] mx-auto rounded-[12px] overflow-hidden'>
+      <div className='relative w-full max-w-[1050px] max-h-[300px] mx-auto rounded-[12px] overflow-hidden'>
         <Swiper
           modules={[Navigation, Autoplay]}
           spaceBetween={0}
@@ -57,26 +52,28 @@ export default function MainBannerSwiper() {
           onSlideChange={(swiper) => setCurrentIndex(swiper.realIndex + 1)}
           className='w-full h-[400px] [&_.swiper-button-prev]:hidden [&_.swiper-button-next]:hidden'
         >
-          {sampleBannerList &&
-            sampleBannerList.map((_, index) => (
+          {mainBannerList &&
+            mainBannerList.map((banner, index) => (
               <SwiperSlide key={index} className='flex justify-center items-center'>
-                <Image src={SampleBannerImg} alt='sample banner image' className='w-full h-full object-cover' />
+                <Link href={banner.banner_link}>
+                  <Image src={banner.banner_image_url} width={1050} height={300} alt={`${banner.banner_name} banner image`} className='w-full h-auto max-w-[1050px] max-h-[300px] object-cover' />
+                </Link>
               </SwiperSlide>
             ))}
         </Swiper>
 
-        <div className='absolute bottom-4 right-4 flex items-center gap-2 z-10'>
+        <div className='absolute bottom-4 right-4 flex items-center gap-1 z-10'>
           {/* ✅ 일시정지/재생 버튼 */}
-          <button className='flex justify-center items-center w-10 h-10 text-white bg-black/40 rounded-[5px]' onClick={toggleAutoplay}>
+          <button className='flex justify-center items-center w-10 h-10 text-white bg-black/50 rounded-[5px]' onClick={toggleAutoplay}>
             {isPlaying ? <LucideIcons.Pause size={24} /> : <LucideIcons.Play size={24} />}
           </button>
 
-          <div className='flex items-center gap-2 px-3 py-1 text-body-16m text-white bg-black/40 rounded-[5px]'>
+          <div className='flex items-center gap-1 px-3 py-1 text-body-16m text-white bg-black/50 rounded-[5px]'>
             <button className='custom-swiper-prev flex justify-center items-center w-8 h-8'>
               <LucideIcons.ChevronLeft size={24} />
             </button>
             <span className='w-[55px] text-center'>
-              {currentIndex} / {totalSlides}
+              {currentIndex} / {mainBannerList?.length}
             </span>
             <button className='custom-swiper-next w-8 h-8 flex items-center justify-center'>
               <LucideIcons.ChevronRight size={24} />
@@ -100,16 +97,15 @@ export default function MainBannerSwiper() {
 
             <div className='overflow-y-auto flex-grow p-4'>
               <div className='grid grid-cols-2 gap-4'>
-                {sampleBannerList &&
-                  sampleBannerList.map((banner) => (
-                    // 외부 링크는 a태그, 내부 링크는 Link 태그
-                    <a key={banner.id} href={banner.url} target='_blank' rel='noopener noreferrer' className='rounded-[8px] cursor-pointer'>
-                      <Image src={banner.image} width={700} alt='sample banner image' />
+                {mainBannerList &&
+                  mainBannerList.map((banner, index) => (
+                    <a key={index} href={banner.banner_link} target='_blank' rel='noopener noreferrer' className='rounded-[8px] cursor-pointer'>
+                      <Image src={banner.banner_image_url} width={700} height={300} alt='sample banner image' className='border border-solid border-gray-200' />
                     </a>
                   ))}
               </div>
 
-              {!sampleBannerList && <p className='text-body-18m text-ui-text-description'>현재 등록되어 있는 이벤트가 없습니다.</p>}
+              {!mainBannerList && <p className='text-body-18m text-ui-text-description'>현재 등록되어 있는 이벤트가 없습니다.</p>}
             </div>
           </div>
         </div>
--- src/components/Banner/SideBanner.tsx
+++ src/components/Banner/SideBanner.tsx
@@ -6,17 +6,17 @@ import Image from 'next/image';
 import { useGetBannerList } from 'src/app/(main)/_hooks/react-query/useGetBannerList';
 
 export default function SideBanner() {
-  const { data: sideBanner, isLoading } = useGetBannerList('side');
+  const { data: sideBannerList, isLoading } = useGetBannerList('side');
 
   return (
     <div className='relative w-[200px] h-[300px] overflow-hidden rounded-[12px] bg-gray-50'>
-      {isLoading || !sideBanner?.data || sideBanner.data.length === 0 ? (
+      {isLoading || !sideBannerList || sideBannerList.length === 0 ? (
         <div className='absolute inset-0 flex justify-center items-center w-full h-full bg-gray-50'>
           <p>등록된 이미지가 없습니다.</p>
         </div>
       ) : (
-        <Link href='#'>
-          <Image src={sideBanner.data[0].banner_image_url} alt='banner image' width={200} height={300} className='absolute inset-0 w-full h-full object-cover' />
+        <Link href={sideBannerList[0].banner_link}>
+          <Image src={sideBannerList[0].banner_image_url} alt='banner image' width={200} height={300} className='absolute inset-0 w-full h-full object-cover' />
         </Link>
       )}
     </div>
