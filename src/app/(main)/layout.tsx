c9e26e0a1ca1006856b7aa84a0a9e1448dc1b202
juny-0429 <juny_0429@naver.com>
2025-03-14 13:30:10 +0900

    feat: showHide,poup banner api 연결

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import PopupBanner from 'src/components/Banner/PopupBanner';
+import ShowHideBanner from 'src/components/Banner/ShowHideBanner';
 import AppLayout from 'src/components/Layout/AppLayout';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <AppLayout>{children}</AppLayout>;
+  return (
+    <AppLayout>
+      {children}
+      <ShowHideBanner />
+      <PopupBanner />
+    </AppLayout>
+  );
 }
