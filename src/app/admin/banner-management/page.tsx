6c6a0eb5fbaf35859b99aa548baf5f371fb6c4ba
juny-0429 <juny_0429@naver.com>
2025-03-20 20:55:34 +0900

    feat: 배너 관리 페이지 완성

--- src/app/admin/banner-management/page.tsx
+++ src/app/admin/banner-management/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import React, { useState } from 'react';
-import Image from 'next/image';
 import Button from 'src/components/Button/Button';
-import { Switch } from 'src/components/Switch/Switch';
-import SampleBannerImg from '@/assets/images/sample-banner-1.png';
-import { useAlertModal, useCustomModal } from 'src/hooks/useModal';
-import AddBannerModalContent from './_components/AddBannerModalContent';
+import { useCustomModal } from 'src/hooks/useModal';
+import AddBannerModalContent from './_components/AddBannerModalContent/AddBannerModalContent';
 import SideBannerForm from './_components/SideBannerForm';
 import DualBannerForm from './_components/DualBannerForm';
 import TripleBannerForm from './_components/TripleBaannerForm';
@@ -14,7 +10,6 @@ import CategoryBannerForm from './_components/CategoryBannerForm';
 import MainBannerForm from './_components/MainBannerForm';
 
 export default function BannerManagementPage() {
-  const { openAlertModal } = useAlertModal();
   const { openCustomModal } = useCustomModal();
 
   const onAddBannerModal = () => {
