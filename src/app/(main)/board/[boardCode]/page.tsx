9b23bd1e67eaeebb95006710f9dcd9fb134c1590
juny-0429 <juny_0429@naver.com>
2025-03-22 02:21:29 +0900

    refactor: 게시판 목록 페이지를 동적 라우팅으로 수정 & 파일 이동

--- src/app/(main)/board/[boardCode]/page.tsx
+++ src/app/(main)/board/[boardCode]/page.tsx
@@ -7,20 +7,18 @@ import TextInput from 'src/components/TextInput/TextInput';
 import Button from 'src/components/Button/Button';
 import { appRoutes } from 'src/routes/appRoutes';
 import Link from 'next/link';
-import { BoardTableColumn } from 'src/types/table/boardTable.types';
-import { boardColumns } from '../_data/boardColumns.data';
+import { boardColumns } from './_data/boardColumns.data';
+import { useGetPostList } from './_hooks/react-qeury/useGetPostList';
+import { useParams } from 'next/navigation';
+import { BoardCategoryType } from 'src/types/boardCategory.types';
 
-const mockData: BoardTableColumn[] = [
-  { id: 1, title: '첫 번째 게시글', userId: 'user_001', createdAt: '2025-03-05' },
-  { id: 2, title: 'React Table 적용', userId: 'user_002', createdAt: '2025-03-04' },
-  { id: 3, title: '게시판 UI 퍼블리싱', userId: 'admin', createdAt: '2025-03-03' },
-];
-
-export default function EventBoardPage() {
+export default function BoardPage() {
+  const { boardCode } = useParams();
+  const { data: eventPostList } = useGetPostList((boardCode as string)?.toUpperCase() as BoardCategoryType);
   const [sorting, setSorting] = useState<SortingState>([]);
 
   const table = useReactTable({
-    data: mockData,
+    data: eventPostList ?? [],
     columns: boardColumns,
     state: { sorting },
     getCoreRowModel: getCoreRowModel(),
@@ -65,7 +63,7 @@ export default function EventBoardPage() {
             table.getRowModel().rows.map((row) => (
               <TableRow key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} style={{ width: cell.column.getSize() }}>
+                  <TableCell key={cell.id} style={{ width: cell.column.getSize() }} className={cell.column.id === 'postTitle' ? 'text-left' : 'text-center'}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
--- src/types/table/boardTable.types.ts
+++
@@ -1,6 +0,0 @@
-export interface BoardTableColumn {
-  id: number;
-  title: string;
-  userId: string;
-  createdAt: string;
-}
