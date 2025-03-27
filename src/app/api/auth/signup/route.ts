64d7f472925281eb51546904f07a57d982ee96d6
juny-0429 <juny_0429@naver.com>
2025-03-27 19:49:33 +0900

    refactor: 회원 가입 시, 사용자 기본 권한 부여

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
     if (error) return NextResponse.json({ error: error.message }, { status: 400 });
 
     // 회원 정보 테이블에 저장
-    const { data: insertData, error: insertError } = await supabase.from('user').insert([
+    const { error: insertError } = await supabase.from('user').insert([
       {
         user_id: user?.id,
         account_id: id,
@@ -33,6 +33,15 @@ export async function POST(request: Request) {
     ]);
 
     if (insertError) return NextResponse.json({ error: insertError.message }, { status: 400 });
+
+    const { error: authorityError } = await supabase.from('authority_log').insert([
+      {
+        auth_id: 1, // 일반 사용자
+        user_id: user!.id,
+      },
+    ]);
+
+    if (authorityError) return NextResponse.json({ error: authorityError.message }, { status: 400 });
 
     return NextResponse.json({ message: '회원가입 완료!', user });
   } catch (error) {
