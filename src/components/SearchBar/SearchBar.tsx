f91c3f90f6f8d65105b45f97db701d24fe498df2
juny-0429 <juny_0429@naver.com>
2025-04-05 19:48:11 +0900

    refactor: 검색 아이콘으로 검색 및 엔터로 검색 추가

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,33 @@
-import { forwardRef } from 'react';
+import { forwardRef, KeyboardEvent } from 'react';
 import TextInput, { TextInputProps } from '../TextInput/TextInput';
 import LucideIcons from 'src/theme/lucideIcon';
 
-interface SearchBarProps extends Omit<TextInputProps, 'isError' | 'rightIcon'> {}
+interface SearchBarProps extends Omit<TextInputProps, 'isError' | 'rightIcon'> {
+  onSearch?: () => void;
+}
 
-const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({ className, ...restProps }, ref) => {
-  return <TextInput type='search' ref={ref} {...restProps} rightIcon={<LucideIcons.Search />} className={className} />;
+const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({ className, onSearch, onKeyDown, ...restProps }, ref) => {
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSearch?.();
+    }
+    onKeyDown?.(e);
+  };
+
+  return (
+    <TextInput
+      type='search'
+      ref={ref}
+      {...restProps}
+      onKeyDown={handleKeyDown}
+      rightIcon={
+        <button type='button' onClick={onSearch}>
+          <LucideIcons.Search />
+        </button>
+      }
+      className={className}
+    />
+  );
 });
 
 SearchBar.displayName = 'SearchBar';
