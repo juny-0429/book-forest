e97208e439ce150e823b095cda993067678aa7bc
juny-0429 <juny_0429@naver.com>
2025-04-07 04:36:37 +0900

    refactor: 아이콘 추가 및 스타일 수정

--- src/app/(main)/_components/CategoryNavigationBar.tsx
+++ src/app/(main)/_components/CategoryNavigationBar.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
-import { mockCategoryNavigationList } from '../_data/CategoryNavigationList';
-import LucideIcons from 'src/theme/lucideIcon';
+import { categoryNavigationList } from '../_data/CategoryNavigationList';
+import Image from 'next/image';
 
 export default function CategoryNavigationBar() {
   return (
     <nav>
-      <ul className='flex justify-center items-center gap-[50px] mt-10'>
-        {mockCategoryNavigationList.map((navItem) => (
-          <li key={navItem.label}>
-            <Link href={navItem.url} className='flex flex-col justify-center items-center gap-3'>
-              <div className='flex justify-center items-center w-[70px] h-[70px] bg-gray-200 rounded-[10px]'>
-                <LucideIcons.Home size={35} className='text-ui-cta' />
+      <ul className='flex justify-center items-center gap-[40px] mt-10'>
+        {categoryNavigationList.map((category) => (
+          <li key={category.label}>
+            <Link href={category.url} className='flex flex-col justify-center items-center gap-3 group'>
+              <div className='flex justify-center items-center w-[100px] h-[100px] bg-gray-200 rounded-[10px]'>
+                <Image src={category.icon} width={80} height={80} alt='category icon' className='transition-transform duration-200 group-hover:scale-110' />
               </div>
-              <span className='text-body-16m text-ui-text-title'>{navItem.label}</span>
+              <span className='text-body-18m text-ui-text-title'>{category.label}</span>
             </Link>
           </li>
         ))}
--- src/app/(main)/_data/CategoryNavigationList.ts
+++ src/app/(main)/_data/CategoryNavigationList.ts
@@ -1,44 +1,60 @@
 import { appRoutes } from 'src/routes/appRoutes';
+import NovelIcon from '@/assets/icons/categories/novel-icon.png';
+import PoetryEssayIcon from '@/assets/icons/categories/poetry-essay-icon.png';
+import HumanitiesIcon from '@/assets/icons/categories/humanities-icon.png';
+import HealthIcon from '@/assets/icons/categories/health-icon.png';
+import SelfImprovementIcon from '@/assets/icons/categories/self-improvement-icon.png';
+import PoliticsSocietyIcon from '@/assets/icons/categories/politics-society-icon.png';
+import ScienceIcon from '@/assets/icons/categories/science-icon.png';
+import ChildrenIcon from '@/assets/icons/categories/children-icon.png';
 
-export const mockCategoryNavigationList = [
+export const categoryNavigationList = [
   {
     label: '소설',
     url: appRoutes.category.novel,
+    icon: NovelIcon,
     isActive: true,
   },
   {
     label: '시/에세이',
     url: appRoutes.category.poetryEssay,
+    icon: PoetryEssayIcon,
     isActive: true,
   },
   {
     label: '인문',
     url: appRoutes.category.humanities,
+    icon: HumanitiesIcon,
     isActive: true,
   },
   {
     label: '건강',
     url: appRoutes.category.health,
+    icon: HealthIcon,
     isActive: true,
   },
   {
     label: '자기계발',
     url: appRoutes.category.selfDevelopment,
+    icon: SelfImprovementIcon,
     isActive: true,
   },
   {
     label: '정치/사회',
     url: appRoutes.category.politicsSociety,
+    icon: PoliticsSocietyIcon,
     isActive: true,
   },
   {
     label: '과학',
     url: appRoutes.category.science,
+    icon: ScienceIcon,
     isActive: true,
   },
   {
     label: '어린이',
     url: appRoutes.category.children,
+    icon: ChildrenIcon,
     isActive: true,
   },
 ];
