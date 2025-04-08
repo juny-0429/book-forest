75abb2933fbe1d98bd7d3b25b4bbd0dd868583d2
juny-0429 <juny_0429@naver.com>
2025-04-09 05:15:20 +0900

    refactor: CategoryToolbar 컴포넌트 분리

--- src/app/(main)/category/[categoryCode]/page.tsx
+++ src/app/(main)/category/[categoryCode]/page.tsx
@@ -1,11 +1,6 @@
 'use client';
 
 import CategoryBanner from 'src/components/Banner/CategoryBanner';
-import LineButton from 'src/components/Button/LineButton';
-import LucideIcons from 'src/theme/lucideIcon';
-import { SelectOption } from 'src/types/select.types';
-import Select from 'src/components/Select/Select';
-import { cn } from 'src/lib/utils';
 import CategoryGridList from './_components/CategoryGridList';
 import CategoryRowList from './_components/CategoryRowList';
 import { useGetCategoryProductList } from './_hooks/react-query/useGetCategoryProductList';
@@ -14,27 +9,22 @@ import CategoryBreadcrumb from './_components/CategoryBreadcrumb';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useGetCategoryPath } from './_hooks/react-query/useGetCategoryPath';
+import CategoryToolbar from './_components/CategoryToolbar';
 
 export default function CategoryPage() {
   const params = useParams();
-  const router = useRouter();
   const searchParams = useSearchParams();
+  const router = useRouter();
   const pathname = usePathname();
   const { ref, inView } = useInView();
 
-  const view = searchParams.get('view') === 'list' ? 'list' : 'grid';
   const categoryCode = params.categoryCode;
+  const view = searchParams.get('view') === 'list' ? 'list' : 'grid';
 
   const { data: categoryPath } = useGetCategoryPath(categoryCode as string);
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetCategoryProductList(categoryCode as string);
 
   const categoryProductList = data?.pages.flatMap((page) => page.categoryProductList) ?? [];
-
-  const options: SelectOption[] = [
-    { value: '', label: '제목순' },
-    { value: '', label: '작가순' },
-    { value: '', label: '가격' },
-  ];
 
   const onUpdateViewType = (type: 'grid' | 'list') => {
     const params = new URLSearchParams(searchParams);
@@ -67,33 +57,12 @@ export default function CategoryPage() {
 
       <CategoryBanner />
 
-      <div className='flex flex-col items-end gap-4 w-full'>
-        <div className='flex justify-center items-center gap-1 w-fit'>
-          <LineButton height={40} color='gray' leftIcon={<LucideIcons.Heart size={20} />}>
-            찜하기
-          </LineButton>
-          <LineButton height={40} color='gray' leftIcon={<LucideIcons.ShoppingCart size={20} />}>
-            카트담기
-          </LineButton>
-          <LineButton height={40}>필터</LineButton>
-        </div>
-
+      <div className='flex flex-col gap-4'>
+        <CategoryToolbar view={view} onUpdateViewType={onUpdateViewType} />
         <hr className='w-full h-[1px] bg-gray-300' />
-
-        <div className='flex justify-center items-center gap-3'>
-          <Select options={options} placeholder='정렬 기준 선택' />
-
-          <button onClick={() => onUpdateViewType('grid')}>
-            <LucideIcons.Grid2X2 size={30} className={cn(view === 'grid' ? 'text-gray-900' : 'text-gray-600')} />
-          </button>
-          <button onClick={() => onUpdateViewType('list')}>
-            <LucideIcons.Menu size={30} className={cn(view === 'list' ? 'text-gray-900' : 'text-gray-600')} />
-          </button>
-        </div>
+        {view === 'grid' && <CategoryGridList categoryProductList={categoryProductList} />}
+        {view === 'list' && <CategoryRowList categoryProductList={categoryProductList} />}
       </div>
-
-      {view === 'grid' && <CategoryGridList categoryProductList={categoryProductList} />}
-      {view === 'list' && <CategoryRowList categoryProductList={categoryProductList} />}
 
       {categoryProductList.length !== 0 && !isFetchingNextPage && <div ref={ref} className='h-[1px]' />}
     </div>
