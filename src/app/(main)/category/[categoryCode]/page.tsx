ecf3d1430e9757e976a8066764dc427e767039c2
juny-0429 <juny_0429@naver.com>
2025-04-11 02:09:22 +0900

    refactor: 카테고리 페이지 동적 라우팅에 따른 meta 설정

--- src/app/(main)/category/[categoryCode]/page.tsx
+++ src/app/(main)/category/[categoryCode]/page.tsx
@@ -1,70 +1,26 @@
-'use client';
+import { Metadata } from 'next';
+import { createSupabaseServer } from 'src/lib/supabaseServer';
+import CategoryPage from './CategoryPage';
 
-import CategoryBanner from 'src/components/Banner/CategoryBanner';
-import CategoryGridList from './_components/CategoryGridList';
-import CategoryRowList from './_components/CategoryRowList';
-import { useGetCategoryProductList } from './_hooks/react-query/useGetCategoryProductList';
-import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
-import CategoryBreadcrumb from './_components/CategoryBreadcrumb';
-import { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { useGetCategoryPath } from './_hooks/react-query/useGetCategoryPath';
-import CategoryToolbar from './_components/CategoryToolbar';
+type Props = {
+  params: { categoryCode: string };
+};
 
-export default function CategoryPage() {
-  const params = useParams();
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-  const { ref, inView } = useInView();
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const supabase = await createSupabaseServer();
+  const { data, error } = await supabase.from('category').select('category_name').eq('category_code', params.categoryCode).single();
 
-  const categoryCode = params.categoryCode;
-  const view = searchParams.get('view') === 'list' ? 'list' : 'grid';
+  if (error || !data) {
+    return {
+      title: '카테고리 정보를 불러올 수 없습니다 | 책숲',
+    };
+  }
 
-  const { data: categoryPath } = useGetCategoryPath(categoryCode as string);
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetCategoryProductList(categoryCode as string);
-
-  const categoryProductList = data?.pages.flatMap((page) => page.categoryProductList) ?? [];
-
-  const onUpdateViewType = (type: 'grid' | 'list') => {
-    const params = new URLSearchParams(searchParams);
-    params.set('view', type);
-    router.push(`?${params.toString()}`);
+  return {
+    title: `${data.category_name} | 책숲`,
   };
+}
 
-  useEffect(() => {
-    const view = searchParams.get('view');
-    if (!view) {
-      const newUrl = `${pathname}?view=grid`;
-      router.replace(newUrl);
-    }
-  }, [searchParams, pathname, router]);
-
-  useEffect(() => {
-    if (inView && hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, isFetchingNextPage]);
-
-  return (
-    <div className='flex flex-col gap-10 w-full'>
-      {categoryPath && (
-        <div className='space-y-2'>
-          <h2 className='text-title-32b text-ui-text-title'>{categoryPath[categoryPath.length - 1]?.name ?? ''}</h2>
-          <CategoryBreadcrumb categoryPath={categoryPath} />
-        </div>
-      )}
-
-      <CategoryBanner />
-
-      <div className='flex flex-col gap-4'>
-        <CategoryToolbar view={view} onUpdateViewType={onUpdateViewType} />
-        <hr className='w-full h-[1px] bg-gray-300' />
-        {view === 'grid' && <CategoryGridList categoryProductList={categoryProductList} />}
-        {view === 'list' && <CategoryRowList categoryProductList={categoryProductList} />}
-      </div>
-
-      {categoryProductList.length !== 0 && !isFetchingNextPage && <div ref={ref} className='h-[1px]' />}
-    </div>
-  );
+export default function Page({ params }: Props) {
+  return <CategoryPage categoryCode={params.categoryCode} />;
 }
