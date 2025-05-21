1999a4cc0777cb31a72cbe5947298f946dcab37f
juny-0429 <juny_0429@naver.com>
2025-05-22 08:02:44 +0900

    fix: 사용하지 않는 error 제외

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -2,7 +2,7 @@ export const copyToClipboard = async (text: string) => {
   try {
     await navigator.clipboard.writeText(text);
     alert('URL이 복사되었습니다!');
-  } catch (err) {
+  } catch (_error) {
     alert('URL 복사에 실패했습니다.');
   }
 };
