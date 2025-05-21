6afdae224ae75271d61cc88b21cdb49573e456fd
juny-0429 <juny_0429@naver.com>
2025-05-22 08:02:12 +0900

    fix: fix: 모달 타입 캐스팅을 통해 Zustand 상태 타입 오류 해결

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,21 +1,21 @@
 import { create } from 'zustand';
 
-interface ModalState<TProps> {
+interface ModalState<TProps = unknown> {
   Component: React.ComponentType<TProps>;
   props?: Omit<TProps, 'onClose'>;
 }
 
 interface ModalStore {
-  modals: ModalState<any>[];
+  modals: ModalState<unknown>[];
   openModal: <TProps>(Component: React.ComponentType<TProps>, props?: Omit<TProps, 'onClose'>) => void;
-  closeModal: (Component: React.ComponentType<any>) => void;
+  closeModal: (Component: React.ComponentType<unknown>) => void;
 }
 
 export const useModalStore = create<ModalStore>((set) => ({
   modals: [],
   openModal: (Component, props) =>
     set((state) => ({
-      modals: [...state.modals, { Component, props }],
+      modals: [...state.modals, { Component: Component as React.ComponentType<unknown>, props } as ModalState<unknown>],
     })),
   closeModal: (Component) =>
     set((state) => ({
