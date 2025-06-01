2b9375b184321e143547f4bb31ead04cd116a893
juny-0429 <juny_0429@naver.com>
2025-06-01 17:44:55 +0900

    fix: 비로그인 장바구니 useEffect 무한 렌더링 이슈 해결

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -13,28 +13,25 @@ export const middleware = async (request: NextRequest) => {
     },
   });
 
-  const token = request.cookies.get('sb-ookfvortvsnjnubjfwgk-auth-token')?.value;
   const path = request.nextUrl.pathname;
   const isAdminRoute = path.startsWith('/admin');
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   // 로그인 상태만 접근 가능
-  if (loggedInRoutes.some((route) => (typeof route === 'string' ? path.startsWith(route) : route.test(path))) && !token) {
+  if (loggedInRoutes.some((route) => (typeof route === 'string' ? path.startsWith(route) : route.test(path))) && !user) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
   // 로그인하면 접근하면 안 되는 페이지
-  if (loggedOutRoutes.includes(path) && token) {
+  if (loggedOutRoutes.includes(path) && user) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
   // 관리자만 접근 가능
-  if (isAdminRoute && token) {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-
-    if (!user) return NextResponse.redirect(new URL('/login', request.url));
-
+  if (isAdminRoute && user) {
     const { data: authData } = await supabase.from('authority_log').select('authority:auth_id(auth_code)').eq('user_id', user.id).single();
 
     const userAuthority = authData?.authority.auth_code;
