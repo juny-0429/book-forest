8429fe9ae68c75474246f367b2b3130cb9064ac6
juny-0429 <juny_0429@naver.com>
2025-06-02 12:17:29 +0900

    refactor: 관리자 프로필 닉네임 및 직급 api 연결

--- src/app/admin/_components/AdminProfileBox.tsx
+++ src/app/admin/_components/AdminProfileBox.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
-import SampleUserImg from '@/assets/images/author/이슬아.png';
 import LucideIcons from 'src/theme/lucideIcon';
 import IconButton from 'src/components/Button/IconButton';
+import { useAuth } from 'src/provider/authProvider';
+import { useGetUserProfile } from 'src/app/(main)/shop/_hooks/react-query/useGetUserProfile';
 
 export default function AdminProfileBox() {
+  const { authority } = useAuth();
+  const { data: userInfo } = useGetUserProfile();
+
   return (
     <section className='flex flex-col items-center gap-4 w-[170px] px-5 py-4 border border-solid border-gray-300 rounded-[6px]'>
-      <figure className='relative'>
-        <div className=' w-[100px] h-[100ox] rounded-full overflow-hidden'>
-          <Image src={SampleUserImg} alt='user profile' />
-        </div>
-
-        <button className='absolute bottom-0 right-0 flex justify-center items-center w-7 h-7 bg-gray-600 rounded-full'>
-          <LucideIcons.Camera size={16} className='text-white' />
-        </button>
-      </figure>
-
       <div className='flex justify-between items-center w-full'>
-        <p className='text-body-16b text-ui-text-title'>admin_juny</p>
+        <p className='text-body-16b text-ui-text-title'>{userInfo?.accountId}</p>
 
         <IconButton height={24}>
           <LucideIcons.Settings size={16} strokeWidth={1} className='text-gray-900' />
@@ -27,7 +20,10 @@ export default function AdminProfileBox() {
 
       <hr className='w-full h-[1px] bg-slate-300' />
 
-      <p className='text-body-16r text-ui-text-description'>일반관리자</p>
+      <div>
+        <span>직급 : </span>
+        <span className='text-body-16r text-ui-text-description'>{authority}</span>
+      </div>
     </section>
   );
 }
